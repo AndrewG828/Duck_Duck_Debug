50965f2508bbcb70dee2c92ecfd5c5e75b353f29
Fanhao Yu <fy229@cornell.edu>
2025-03-29 12:54:07 -0400

    Setup frontend

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,10 @@
+// src/App.jsx
+import React, { useState } from "react";
+import DuckDuckDebug from "./Debugger";
+import "./styles.css";
+
+const App = () => {
+  return <DuckDuckDebug />;
+};
+
+export default App;
